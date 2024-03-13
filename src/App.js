e3111dcb9966b263a9f5105d1c1d1997141615a7
unknown <hnayak0914@gmail.com>
2024-03-12 20:21:53 -0400

    fixed typo, changed color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Navbar from "./components/navbar/navbar";
 import background from "./gym1.jpg";
 import "./app.css";
@@ -8,26 +7,34 @@ import exercises from "./exercises.json";
 export default function App() {
   return (
     <>
-    <div className="imgonnakms">
-      <Navbar></Navbar>
-      <div className="home" style={{backgroundImage: `url(${background})`}}>
-      <div className="title">ACHEIVE YOUR DREAM PHYSIQUE</div>
-      <button className="beginner-button"formAction="submit">BEGINNER WORKOUT PLAN</button>
-      </div>
-      <h2 className="header" id="repo">Exercise Repository</h2>
-      <div className="exercises">
-        {exercises.map((exercise) => (
-          <Card
-            name={exercise.name}
-            link={exercise.link}
-            numberReps={exercise.numberReps}
-            numberSets={exercise.numberSets}
-            notes={exercise.notes}
-            target={exercise.target}
-          />
-        ))}
-      </div>
-      <h2 className="header" id="repo">Beginner Workout Plan</h2>
+      <div className="imgonnakms">
+        <Navbar></Navbar>
+        <div className="home" style={{ backgroundImage: `url(${background})` }}>
+          <div className="title">ACHIEVE YOUR DREAM PHYSIQUE</div>
+          <div>
+            <button className="beginner-button" formAction="submit">
+              BEGINNER WORKOUT PLAN
+            </button>
+          </div>
+        </div>
+        <h2 className="header" id="repo">
+          Exercise Repository
+        </h2>
+        <div className="exercises">
+          {exercises.map((exercise) => (
+            <Card
+              name={exercise.name}
+              link={exercise.link}
+              numberReps={exercise.numberReps}
+              numberSets={exercise.numberSets}
+              notes={exercise.notes}
+              target={exercise.target}
+            />
+          ))}
+        </div>
+        <h2 className="header" id="repo">
+          Beginner Workout Plan
+        </h2>
       </div>
     </>
   );
---
+++ src/components/pushDay.js
@@ -0,0 +1,7 @@
+export default function Push() {
+  return (
+    <>
+      <h2 className="sub-header">Push Day</h2>
+    </>
+  );
+}
